429bcf3db54581a551533d36211e955213f26fed
HenryLiu714 <henryliu714@gmail.com>
2022-04-12 07:47:12 -0400

    start 4/12

--- app/src/main/java/com/example/employbee/EditTasks.java
+++ app/src/main/java/com/example/employbee/EditTasks.java
@@ -12,14 +12,17 @@ public class EditTasks {
     private static Cell cell;
     private static Sheet sheet;
 
-    // fileName is "SampleSheet.xls" hypothetically speaking
-    public static ArrayList<Task> getTasks(Context context, String fileName) throws IOException {
+    // must call before using other methods (OnCreate)
+    public static void getSheet(Context context, String fileName) throws IOException{
         File file = new File(context.getExternalFilesDir(null), fileName);
         FileInputStream inputStream = new FileInputStream(file);
 
         Workbook workbook = new HSSFWorkbook(inputStream);
         sheet = workbook.getSheetAt(0);
+    }
 
+    // fileName is "SampleSheet.xls" hypothetically speaking
+    public static ArrayList<Task> getTasks() throws IOException {
         ArrayList<Task> tasks = new ArrayList<Task>();
         for (Row row: sheet) {
             Iterator<Cell> cellIterator = row.cellIterator();
@@ -37,5 +40,26 @@ public class EditTasks {
         }
 
         return tasks;
+    }
+
+    // adds a new task
+    public static void addTask(Task task, Context context, String fileName) throws IOException {
+        addTaskHelper(task);
+
+        File file = new File(context.getExternalFilesDir(null), fileName);
+        FileOutputStream outputStream = new FileOutputStream(file);
+        Workbook workbook = new HSSFWorkbook(new FileInputStream(file));
+        workbook.write(outputStream);
+
+    }
+
+    private static void addTaskHelper(Task task) {
+        int lastRow = sheet.getLastRowNum();
+        Row row = sheet.createRow(lastRow + 1);
+
+        row.createCell(0).setCellValue(task.getTask());
+        row.createCell(1).setCellValue(task.getShift());
+        row.createCell(2).setCellValue(task.getPos());
+        row.createCell(3).setCellValue(task.getDone());
     }
 }
